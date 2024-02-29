bc6dbe0f23ad9fb16256fd6966dc49808b52f018
haithembourbia <haithem.bourbia@dipower.fr>
2024-02-29 20:46:06 +0100

    calculeSomme & calculeMoy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import HeithemBourbia.HeithemBourbia;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] tableau = {1, 2, 3, 4, 5};
+
+        // Appel des méthodes de la classe Membre1
+        int somme = HeithemBourbia.calculerSomme(tableau);
+        double moyenne = HeithemBourbia.calculerMoyenne(tableau);
+
+        // Affichage des résultats
+        System.out.println("Somme du tableau: " + somme);
+        System.out.println("Moyenne du tableau: " + moyenne);
     }
 }
