778f07651d18a8be2339f223c6be48b38ee2525e
wejden.hadded <wejden.hadded@dpc.com.tn>
2024-03-01 19:50:10 +0100

    add calcul factoriel method & isEven method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import static wissemBoumahdef.WissemBoumahdef.calculateFactorial;
+import static wissemBoumahdef.WissemBoumahdef.isEven;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int number = 4;
+        int factorial = calculateFactorial(number);
+        System.out.println("Factorielle de " + number + " : " + factorial);
+
+        // Vérification si 4 est pair
+        boolean isEven = isEven(number);
+        if (isEven) {
+            System.out.println(number + " est pair.");
+        } else {
+            System.out.println(number + " n'est pas pair.");
+        }
+
     }
 }
