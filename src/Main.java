dcc74ad485d3388c5d9b0723ad3043d8f3de5885
haithembourbia <haithem.bourbia@dipower.fr>
2024-03-01 20:27:04 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ import HeithemBourbia.HeithemBourbia;
 import static wissemBoumahdef.WissemBoumahdef.calculateFactorial;
 import static wissemBoumahdef.WissemBoumahdef.isEven;
 
+
 public class Main {
     public static void main(String[] args) {
-        int[] tableau = {1, 2, 3, 4, 5};
+        int[] tableau = {1, 2, 3, 4, 5, 6, 7};
 
         // Appel des méthodes de la classe Membre1
         int somme = HeithemBourbia.calculerSomme(tableau);
