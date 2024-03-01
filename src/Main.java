bafa8665638ead6b6afe7340e3796fb8afc38129
wejden.hadded <wejden.hadded@dpc.com.tn>
2024-03-01 22:07:12 +0100

    change table size

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import static wissemBoumahdef.WissemBoumahdef.isEven;
 public class Main {
     public static void main(String[] args) {
 
-        int[] tableau = {1, 2, 3, 5, 6};
+        int[] tableau = {1, 2, 3, 5, 6,8,9};
 
         // Appel des méthodes de la classe Membre1
         int somme = HeithemBourbia.calculerSomme(tableau);
